452174dbf553e6198cd35937aada4ce52bb79d95
Benjamin Karg <benjamin.karg@tu-berlin.de>
2018-04-03 15:28:46 +0200

    two new examples initalized

---
+++ examples/energy_management_system.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.insert(0,'../lib')
+sys.path.insert(0,'../systems')
+import numpy as np
+import scipy.io as sio
+from edgeAI_classes import *
+from energy_management_system import *
+import pdb
+
+### parameter/options
+code = 'c'
+project_name = 'energy_management_system'
+N = 12
+max_iter = 2000
+opt = edgeAI_Options(code,project_name,N,max_iter)
+
+### system
+sys = energy_management_system()
+
+### problem
+prob = None # for control with deep neural network
+
+### neural networks
+nn = edgeAI_NeuralNetwork("example")
+
+### external data
+data = sio.loadmat('data/wdata_processed.mat')
+extdata = edgeAI_ExtData(data["extdata"])
+
+### create edgeAI
+edgy = edgeAI(opt,sys,prob,nn,extdata)
+
+# choose which parts of the code should be generated
+solver = 'nn' # either nn or qp
+simulation = True
+edgy.generate_code_mpc(solver,simulation)
---
+++ examples/temperature_control.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.insert(0,'../lib')
+sys.path.insert(0,'../systems')
+import numpy as np
+import scipy.io as sio
+from edgeAI_classes import *
+from energy_management_system import *
+import pdb
+
+### parameter/options
+code = 'c'
+project_name = 'energy_management_system'
+N = 12
+max_iter = 2000
+opt = edgeAI_Options(code,project_name,N,max_iter)
+
+### system
+sys = energy_management_system()
+
+### problem
+prob = edgeAI_Problem(sys,opt)
+
+### neural networks
+nn = None
+
+### external data
+data = sio.loadmat('data/wdata_processed.mat')
+extdata = edgeAI_ExtData(data["extdata"])
+
+### create edgeAI
+edgy = edgeAI(opt,sys,prob,nn,extdata)
+
+# choose which parts of the code should be generated
+solver = 'qp'
+simulation = True
+edgy.generate_code_mpc(solver,simulation)
