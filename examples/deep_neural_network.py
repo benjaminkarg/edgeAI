545973a793e627e469b52db144b0cea8e97ffd4e
Benjamin Karg <benjamin.karg@tu-berlin.de>
2018-04-03 15:27:26 +0200

    example one renamed: deep_neural_network

---
+++ examples/deep_neural_network.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.insert(0,'../lib')
+sys.path.insert(0,'../systems')
+import numpy as np
+import scipy.io as sio
+from edgeAI_classes import *
+from room_temp_model import *
+
+### parameter/options
+code = 'c'
+project_name = 'deep_neural_network'
+opt = edgeAI_Options(code,project_name)
+
+### neural networks
+nn = edgeAI_NeuralNetwork("example")
+
+### create edgeAI
+edgy = edgeAI(opt,neuralnetwork=nn)
+
+# generate code
+device = 'fpga' # either "micro" for microcontroller or "fpga" for FPGA
+edgy.generate_code_dnn(device)
